fa83a45caa5dac7e12f6303414667131f2f54f06
Mansour Hasanzaee <mansourhasanzaee@aut.ac.ir>
2024-09-04 02:44:56 +0100

    fix: removing some extra lines.
    there are still a dozen of bugs, but  I see you as the one who can fix them.

--- Password Generator.py
+++ Password Generator.py
@@ -1,29 +1,14 @@
 from random import choice, randint
+import string
 
-
-lowercase_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-uppercase_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['`', '~', '@', '#', '%', '^', '*', '-', '_', '=', '!', '$', '&', '(', ')', '+', '{', '}', '[', ']', '|', '\\', ':', ';', '/', '"', "'", '<', '>', '?', '.']
-
+characters = string.ascii_letters + string.digits + string.punctuation
 
 len_of_password = int(input("Enter the length of your desired password: "))
 password = []
+
 def password_generator():
     for i in range(1, len_of_password+1):
-        random_num = randint(1, 4)
-        if random_num == 1:
-            l1 = choice(lowercase_letters)
-            password.append(l1)
-        elif random_num == 2:
-            u2 = choice(uppercase_letters)
-            password.append(u2)
-        elif random_num == 3:
-            n3 = choice(numbers)
-            password.append(n3)
-        else:
-            s4 = choice(symbols)
-            password.append(s4)
+        password.append(choice(characters))
     return "".join(password)
 
 
